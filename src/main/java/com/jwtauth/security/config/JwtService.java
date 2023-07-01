70f569bf0c04e121a7398f11e5555520cc42e1ad
LaViK <lavik@LaViKs-MacBook-Pro.local>
2023-07-02 00:36:40 +0530

    Added 'extractAllClaims' method in 'JwtService.java'

--- src/main/java/com/jwtauth/security/config/JwtService.java
+++ src/main/java/com/jwtauth/security/config/JwtService.java
@@ -1,10 +1,22 @@
 package com.jwtauth.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
     public String extractUsername(String jwtToken) {
         return null;
+    }
+
+    private Claims extractAllClaims(String jwtToken) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJwt(jwtToken)
+                .getBody();
+
     }
 }
