8b9cc339f624e44f4a8b37fd83a86cca2a2b7a78
LaViK <lavik@LaViKs-MacBook-Pro.local>
2023-07-01 16:21:40 +0530

    Implemented User Entity

---
+++ src/main/java/com/jwtauth/security/user/User.java
@@ -0,0 +1,31 @@
+package com.jwtauth.security.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user") // '_user' is the name of our table in DataBase because 'users' table is already maintained by postgresql.
+public class User {
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
