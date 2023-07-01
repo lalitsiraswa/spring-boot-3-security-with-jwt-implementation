b59ff8fe0caf028618edee9a391cd85bdfa47715
LaViK <lavik@LaViKs-MacBook-Pro.local>
2023-07-01 18:15:18 +0530

    Implemented 'JwtAuthenticationFilter' by extending 'OncePerRequestFilter'

---
+++ src/main/java/com/jwtauth/security/config/JwtAuthenticationFilter.java
@@ -0,0 +1,26 @@
+package com.jwtauth.security.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component // @Repository // @Service are same
+@RequiredArgsConstructor
+// '@RequiredArgsConstructor' It will create a constructor using any 'final' field in our class. 'eg: private final String name';
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
