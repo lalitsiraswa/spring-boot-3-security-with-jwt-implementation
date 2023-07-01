cda92474d025ac2aa83f4d954cf985f5ac62847f
LaViK <lavik@LaViKs-MacBook-Pro.local>
2023-07-01 16:22:55 +0530

    Generating the userId 'GenerationType.AUTO'

--- src/main/java/com/jwtauth/security/user/User.java
+++ src/main/java/com/jwtauth/security/user/User.java
@@ -14,15 +14,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "_user") // '_user' is the name of our table in DataBase because 'users' table is already maintained by postgresql.
 public class User {
     @Id
-    @SequenceGenerator(
-            name = "user_sequence",
-            sequenceName = "user_sequence",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "user_sequence"
-    )
+    @GeneratedValue
     private Integer id;
     private String firstname;
     private String lastname;
