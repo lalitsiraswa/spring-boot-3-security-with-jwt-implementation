a499e425bf80459e068017aae33bc8602e048841
LaViK <lavik@LaViKs-MacBook-Pro.local>
2023-07-02 17:15:04 +0530

    Implemented 'AuthenticationProvider' @Bean in 'ApplicationConfig.java'

--- src/main/java/com/jwtauth/security/config/ApplicationConfig.java
+++ src/main/java/com/jwtauth/security/config/ApplicationConfig.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -24,9 +26,25 @@ public class ApplicationConfig {
             }
         };
     }
-//    @Bean
+
+    //    @Bean
 //    public UserDetailsService userDetailsService() {
 //        return username -> userRepository.findByEmail(username)
 //                .orElseThrow(() -> new UsernameNotFoundException("User not found !!"));
 //    }
+
+    //  The AuthenticationProvider is the "DataAccessObject" which is responsible for to fetch the user details and
+    //  also encode password.
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+//        DataAccessObjectAuthenticationProvider (DaoAuthenticationProvider)
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+//      We need to tell this AuthenticationProvider which UserDetails Service to use in oder to fetch information
+//      about our User.
+        authenticationProvider.setUserDetailsService(userDetailsService());
+//      Need to specify which passwordEncoder we are using within our Application.
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
 }
