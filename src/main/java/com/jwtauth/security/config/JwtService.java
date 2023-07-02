65b9d536449ad9d59b6bb00b0caf08a7816b2d19
LaViK <lavik@LaViKs-MacBook-Pro.local>
2023-07-02 12:32:21 +0530

    Implemented 'generateToken' method in 'JwtService.java'

--- src/main/java/com/jwtauth/security/config/JwtService.java
+++ src/main/java/com/jwtauth/security/config/JwtService.java
@@ -2,16 +2,23 @@ package com.jwtauth.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.xml.crypto.Data;
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JwtService {
     private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+    private static final Integer TOKEN_EXPIRY_TIME = 24 * 60 * 60 * 1000;
 
     public String extractUsername(String jwtToken) {
         return extractClaim(jwtToken, Claims::getSubject);
@@ -20,6 +27,24 @@ public class JwtService {
     public <T> T extractClaim(String jwtToken, Function<Claims, T> claimResolver) {
         final Claims claims = extractAllClaims(jwtToken);
         return claimResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + TOKEN_EXPIRY_TIME))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact(); // compact() will generate and return JwtToken String.
     }
 
     private Claims extractAllClaims(String jwtToken) {
