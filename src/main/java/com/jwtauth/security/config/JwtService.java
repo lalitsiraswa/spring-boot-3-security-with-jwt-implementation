005e778b37276a058b02cb0bd78cfcc9ab2665be
LaViK <lavik@LaViKs-MacBook-Pro.local>
2023-07-02 12:10:25 +0530

    Implemented 'extractUsername' method in 'JwtService.java'

--- src/main/java/com/jwtauth/security/config/JwtService.java
+++ src/main/java/com/jwtauth/security/config/JwtService.java
@@ -14,7 +14,7 @@ public class JwtService {
     private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
 
     public String extractUsername(String jwtToken) {
-        return null;
+        return extractClaim(jwtToken, Claims::getSubject);
     }
 
     public <T> T extractClaim(String jwtToken, Function<Claims, T> claimResolver) {
