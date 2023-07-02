0d338a553a6423a88c5a4479717109fb6005db85
LaViK <lavik@LaViKs-MacBook-Pro.local>
2023-07-02 23:06:27 +0530

    Implemented 'register' method in 'AuthenticationService.java' Class

--- src/main/java/com/jwtauth/security/auth/AuthenticationService.java
+++ src/main/java/com/jwtauth/security/auth/AuthenticationService.java
@@ -1,14 +1,35 @@
 package com.jwtauth.security.auth;
 
+import com.jwtauth.security.config.JwtService;
+import com.jwtauth.security.user.Role;
+import com.jwtauth.security.user.User;
+import com.jwtauth.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ProblemDetail;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+    private final UserRepository userRepository; // 'final', So it will be automatically injected by Spring.
+    private final PasswordEncoder passwordEncoder; // 'final', So it will be automatically injected by Spring.
+    private final JwtService jwtService; // 'final', So it will be automatically injected by Spring.
+
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
