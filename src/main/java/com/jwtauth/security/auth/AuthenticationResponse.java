e4f109fcb9aa1dbe72bf2e1df15c8697249efe8d
LaViK <lavik@LaViKs-MacBook-Pro.local>
2023-07-02 22:34:06 +0530

    Implemented 'AuthenticationResponse.java' Class

---
+++ src/main/java/com/jwtauth/security/auth/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.jwtauth.security.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+}
